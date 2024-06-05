cb714e2c50a3832684ed9a9c9a7a2ba963c887ee
ritusingh00501 <ritusingh00501@gmail.com>
2024-06-05 23:10:54 +0530

    First commit

---
+++ Models/BillDetails.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication14.Models;
+
+namespace WebApplication14.Models
+{
+	public class BillDetails
+	{
+		public int id { get; set; }
+		public string custname { get; set; }
+		public int contact { get; set; }
+		public string address { get; set; }
+		public int total { get; set; }
+		public List<Items> Items { get; set; }
+
+		public BillDetails()
+		{
+
+		}
+
+	}
+}
---
+++ Models/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication14.Models
+{
+	public class Items
+	{
+		public int id { get; set; }
+		public string prodname { get; set; }
+		public int price { get; set; }
+		public int quantity { get; set; }
+		public int ItemIndex { get; set; }
+	}
+}
